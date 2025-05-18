2c9520daf7f55e52031534650bde6a5519dc4c55
yuri <yuridomingues.contato@gmail.com>
2025-05-18 17:13:24 -0300

    feat: implement ticket creation and listing with client association

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Clients from "./pages/Clients";
+import Tickets from "./pages/Tickets";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/clients" element={<Clients />} />
+        <Route path="/tickets" element={<Tickets />} />
       </Routes>
     </BrowserRouter>
   );
